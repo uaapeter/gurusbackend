c1492d3744e67cc2b3570b51d3b8f3efd4bff9aa
uaapeter <uaapeter@gmail.com>
2025-01-17 10:15:21 +0000

    dburl

--- connection.js
+++ connection.js
@@ -4,8 +4,8 @@ GLOBAL.mongoose.set('strictQuery', false)
 export const databaseConnection = async () => {
     try {
        
-        // GLOBAL.mongoose.connect(`mongodb+srv://peteraondoyavenga:Johndoe2023@pos-db.agiev.mongodb.net/?retryWrites=true&w=majority&appName=pos-db`);
-        GLOBAL.mongoose.connect(process.env.MONGODB);
+        GLOBAL.mongoose.connect(`mongodb+srv://peteraondoyavenga:Johndoe2023@gurusdb.u5fu2.mongodb.net/?retryWrites=true&w=majority&appName=gurusdb`);
+        // GLOBAL.mongoose.connect(process.env.MONGODB);
     } catch (error) {
         console.log('Error ============');
     }
